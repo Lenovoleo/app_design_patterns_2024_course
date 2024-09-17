5dc4554b2f1a753327889f2e7d4828bd7dde5760
Leo_hsa <public@gmail.com>
2024-09-17 17:40:32 +0500

    finished 1week

--- 1 week/homework1.java
+++ 1 week/homework1.java
@@ -1,3 +1,163 @@
+
 public class homework1 {
-    
+
+    public static void main(String[] args) {
+        
+        Book book1 = new Book("Book One", "Author One", 12345, 5);
+        Book book2 = new Book("Book Two", "Author Two", 23456, 3);
+
+       
+        Reader reader1 = new Reader("John Doe", 1001);
+        Reader reader2 = new Reader("Jane Smith", 1002);
+
+        
+        Library library = new Library(new Book[10], new Reader[10]);
+        library.addBook(book1);
+        library.addBook(book2);
+
+        
+        library.registerReader(reader1);
+        library.registerReader(reader2);
+
+        
+        library.issueBook(0, 1001); 
+        library.issueBook(0, 1002); 
+
+        
+        library.returnBook(0, 1001); 
+    }
+}
+
+class Book {
+    protected String name;
+    protected String author;
+    protected int isbn;
+    protected int num_of_copies;
+    protected int num_of_issued_copies = 0;
+
+    Book(String name, String author, int isbn, int num_of_copies) {
+        this.name = name;
+        this.author = author;
+        this.isbn = isbn;
+        this.num_of_copies = num_of_copies;
+    }
+
+
+    public boolean isAvailable() {
+        return num_of_issued_copies < num_of_copies;
+    }
+
+    public void issue() {
+        if (isAvailable()) {
+            num_of_issued_copies++;
+        } else {
+            System.out.println("No available copies of this book.");
+        }
+    }
+
+ 
+    public void returnBook() {
+        if (num_of_issued_copies > 0) {
+            num_of_issued_copies--;
+        } else {
+            System.out.println("No issued copies to return.");
+        }
+    }
+}
+
+class Reader {
+    protected String name;
+    protected int id_of_reader;
+
+    Reader(String name, int id_of_reader) {
+        this.name = name;
+        this.id_of_reader = id_of_reader;
+    }
+}
+
+class Library {
+    protected Book[] list_of_books;
+    protected int size_list_books = 0;
+    protected Reader[] list_of_readers;
+    protected int size_list_readers = 0;
+
+
+    Library(Book[] list_of_books, Reader[] list_of_readers) {
+        this.list_of_books = list_of_books;
+        this.list_of_readers = list_of_readers;
+    }
+
+  
+    public void addBook(Book book) {
+        if (size_list_books < list_of_books.length) {
+            list_of_books[size_list_books] = book;
+            size_list_books++;
+        } else {
+            System.out.println("Library is full.");
+        }
+    }
+
+
+    public void dropBook(int index_book) {
+        if (index_book < 0 || index_book >= size_list_books) {
+            System.out.println("Invalid index.");
+            return;
+        }
+        Book[] arr = new Book[list_of_books.length - 1];
+        int index = 0;
+        for (int i = 0; i < list_of_books.length; i++) {
+            if (i != index_book) {
+                arr[index++] = list_of_books[i];
+            }
+        }
+        list_of_books = arr;
+        size_list_books--;
+    }
+
+
+    public void registerReader(Reader reader) {
+        if (size_list_readers < list_of_readers.length) {
+            list_of_readers[size_list_readers] = reader;
+            size_list_readers++;
+        } else {
+            System.out.println("Library has reached the maximum number of readers.");
+        }
+    }
+
+
+    public void issueBook(int index_book, int reader_id) {
+        Book book = list_of_books[index_book];
+        Reader reader = findReaderById(reader_id);
+
+        if (reader != null && book != null) {
+            if (book.isAvailable()) {
+                book.issue();
+                System.out.println("Book issued to " + reader.name);
+            } else {
+                System.out.println("No available copies of " + book.name);
+            }
+        }
+    }
+
+
+    public void returnBook(int index_book, int reader_id) {
+        Book book = list_of_books[index_book];
+        Reader reader = findReaderById(reader_id);
+
+        if (reader != null && book != null) {
+            book.returnBook();
+            System.out.println("Book returned by " + reader.name);
+        }
+    }
+
+
+    private Reader findReaderById(int reader_id) {
+        for (int i = 0; i < size_list_readers; i++) {
+            if (list_of_readers[i].id_of_reader == reader_id) {
+                return list_of_readers[i];
+            }
+        }
+        System.out.println("Reader not found.");
+        return null;
+    }
 }
--- 1 week/prac1.java
+++ 1 week/prac1.java
@@ -4,136 +4,181 @@ public class prac1 {
         Car car2 = new Car("Honda", "Civic", 2019, 4, "Manual");
         Motorcycle moto1 = new Motorcycle("Yamaha", "MT-07", 2021, "Naked", false);
 
-       
-        Garage garage1 = new Garage(new Vehicle[10]); 
-        Garage garage2 = new Garage(new Vehicle[5]);  
+        
+        Garage garage1 = new Garage(new Vehicle[10]);
+        Garage garage2 = new Garage(new Vehicle[5]);
 
+       
         garage1.addVehicle(car1);
         garage1.addVehicle(moto1);
         garage2.addVehicle(car2);
+
+    
+        Fleet fleet = new Fleet(new Garage[5]); 
+        fleet.addGarage(garage1);
+        fleet.addGarage(garage2);
+
+       
+        Vehicle foundVehicle = fleet.searchVehicle("Toyota", "Camry");
+        if (foundVehicle != null) {
+            System.out.println("Found vehicle: " + foundVehicle.brand + " " + foundVehicle.model);
+        } else {
+            System.out.println("Vehicle not found");
+        }
     }
 }
 
-abstract class Vehicle{
+
+abstract class Vehicle {
     protected String brand;
     protected String model;
     protected int issue_year;
 
-    public Vehicle(String brand, String model, int issue_year){
+    public Vehicle(String brand, String model, int issue_year) {
         this.brand = brand;
         this.model = model;
         this.issue_year = issue_year;
     }
+
     public abstract void startEngine();
     public abstract void stopEngine();
 }
 
-class Car extends Vehicle{
+
+class Car extends Vehicle {
     protected int amount_doors;
     protected String transmission_type;
 
-    public Car(String brand, String model, int issue_year, int amount_doors, String transmission_type){
+    public Car(String brand, String model, int issue_year, int amount_doors, String transmission_type) {
         super(brand, model, issue_year);
         this.amount_doors = amount_doors;
         this.transmission_type = transmission_type;
     }
 
     @Override
-    public void startEngine(){
-        System.out.println("Engine start");
+    public void startEngine() {
+        System.out.println("Car engine started.");
     }
 
     @Override
-    public void stopEngine(){
-        System.out.println("Engine stop");
+    public void stopEngine() {
+        System.out.println("Car engine stopped.");
     }
 }
 
-class Motorcycle extends Vehicle{
+
+class Motorcycle extends Vehicle {
     protected String body_type;
     protected boolean has_box;
 
-    public Motorcycle(String brand , String model, int issue_year, String body_type, boolean has_box){
-        super(brand , model, issue_year);
+    public Motorcycle(String brand, String model, int issue_year, String body_type, boolean has_box) {
+        super(brand, model, issue_year);
         this.body_type = body_type;
         this.has_box = has_box;
     }
+
     @Override
-    public void startEngine(){
-        System.out.println("Engine start");
+    public void startEngine() {
+        System.out.println("Motorcycle engine started.");
     }
 
     @Override
-    public void stopEngine(){
-        System.out.println("Engine stop");
+    public void stopEngine() {
+        System.out.println("Motorcycle engine stopped.");
     }
-
 }
 
 
-class Garage{
-    private Vehicle list_vehicle[];
+class Garage {
+    protected Vehicle[] list_vehicle;
     private int size_of_vehicles = 0;
 
-    public void addVehicle(Vehicle car){
-        list_vehicle[size_of_vehicles] = car;
-        size_of_vehicles++;
-    }
-
-    Garage(Vehicle list_vehicle[]){
+  
+    Garage(Vehicle[] list_vehicle) {
         this.list_vehicle = list_vehicle;
     }
-    public void dropVehicle(int index_vehicle){
-        Vehicle arr[] = new Vehicle[list_vehicle.length - 1];
+
+   
+    public void addVehicle(Vehicle car) {
+        if (size_of_vehicles < list_vehicle.length) {
+            list_vehicle[size_of_vehicles] = car;
+            size_of_vehicles++;
+        } else {
+            System.out.println("Garage is full, cannot add more vehicles.");
+        }
+    }
+
+    
+    public void dropVehicle(int index_vehicle) {
+        Vehicle[] arr = new Vehicle[list_vehicle.length - 1];
         int index = 0;
 
-        for(int i = 0; i < list_vehicle.length ; i++){
-            if(i != index_vehicle){
+        for (int i = 0; i < size_of_vehicles; i++) {
+            if (i != index_vehicle) {
                 arr[index++] = list_vehicle[i];
             }
         }
         list_vehicle = arr;
-    
-    
+        size_of_vehicles--;
     }
-    public Vehicle[] getVehicles(){
-        return list_vehicle;
+
+  
+    public Vehicle[] getVehicles() {
+        Vehicle[] currentVehicles = new Vehicle[size_of_vehicles];
+        for (int i = 0; i < size_of_vehicles; i++) {
+            currentVehicles[i] = list_vehicle[i];
+        }
+        return currentVehicles;
     }
 }
 
-class Fleet{
-    private Garage list_garages[];
+
+class Fleet {
+    private Garage[] list_garages;
     private int size_list_garages = 0;
 
-    Fleet(Garage list_garages[]){
+    
+    Fleet(Garage[] list_garages) {
         this.list_garages = list_garages;
     }
 
-    public void addGarage(Garage g){
-        list_garages[size_list_garages] = g;
-        size_list_garages++;
+
+    public void addGarage(Garage g) {
+        if (size_list_garages < list_garages.length) {
+            list_garages[size_list_garages] = g;
+            size_list_garages++;
+        } else {
+            System.out.println("Fleet is full, cannot add more garages.");
+        }
     }
 
-    public void dropGarage(int index_garage){
-        Garage arr[] = new Garage[list_garages.length - 1];
+
+    public void dropGarage(int index_garage) {
+        if (index_garage < 0 || index_garage >= size_list_garages) {
+            System.out.println("Invalid index, no garage removed.");
+            return;
+        }
+
+        Garage[] arr = new Garage[list_garages.length - 1];
         int index = 0;
-        for(int i = 0; i < list_garages.length; i++){
-            if(i != index_garage){
-                arr[index] = list_garages[i];
+        for (int i = 0; i < size_list_garages; i++) {
+            if (i != index_garage) {
+                arr[index++] = list_garages[i];
             }
         }
         list_garages = arr;
+        size_list_garages--;
     }
-    public Vehicle searchVehicle(String brand, String model){
-        for (Garage garage : list_garages) {
-            for (Vehicle vehicle : garage.getVehicles()) {  
+
+    
+    public Vehicle searchVehicle(String brand, String model) {
+        for (int i = 0; i < size_list_garages; i++) {
+            for (Vehicle vehicle : list_garages[i].getVehicles()) {
                 if (vehicle.brand.equals(brand) && vehicle.model.equals(model)) {
-                    return vehicle;  
+                    return vehicle;
                 }
             }
         }
-        return null; 
+        return null;
     }
-
-
 }
