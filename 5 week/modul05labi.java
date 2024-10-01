26f458aaa9ea14853bbb1b8909c5b158c0c09fb8
Leo_hsa <public@gmail.com>
2024-10-01 08:50:30 +0500

    finished modul 05

--- 5 week/modul05labi.java
+++ 5 week/modul05labi.java
@@ -3,159 +3,161 @@ import java.util.Scanner;
 public class modul05labi {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
+        int choice;
 
-        System.out.println("Выберите тип транспорта:");
-        System.out.println("1. Автомобиль");
-        System.out.println("2. Мотоцикл");
-        System.out.println("3. Самолет");
-
-        int choice = scanner.nextInt();
-
-        Transport transport = createTransport(choice);
+        System.out.println("Выберите тип транспорта:\n 1.Car \n 2.Moto \n 3.Plane \n 4.Bike");
+        choice = sc.nextInt();
+        sc.nextLine(); 
 
         System.out.println("Введите модель:");
-        String model = scanner.next();
-        transport.setModel(model);
+        String model = sc.nextLine();
 
         System.out.println("Введите скорость:");
-        int speed = scanner.nextInt();
-        transport.setSpeed(speed);
+        int speed = sc.nextInt();
 
-        System.out.println("\nСоздан транспорт:");
-        System.out.println("Тип: " + transport.getClass().getSimpleName());
-        System.out.println("Модель: " + transport.getModel());
-        System.out.println("Скорость: " + transport.getSpeed() + " км/ч");
+        Transport transport = null;
 
-        transport.Move();
-        transport.FuelUp();
-    }
-
-    private static Transport createTransport(int choice) {
         switch (choice) {
             case 1:
-                return new Car();
+                transport = new CarFactory().CreateTransport(model, speed);
+                break;
             case 2:
-                return new Motorcycle();
+                transport = new MotorcycleFactory().CreateTransport(model, speed);
+                break;
             case 3:
-                return new Plane();
+                transport = new PlaneFactory().CreateTransport(model, speed);
+                break;
+            case 4:
+                transport = new BikeFactory().CreateTransport(model, speed);
+                break;
             default:
-                System.out.println("Неверный выбор.");
-                return null;
+                System.out.println("Неверный выбор!");
+                System.exit(0);
+        }
+
+        if (transport != null) {
+            transport.Move();
+            transport.Fuel();
         }
     }
 }
 
 interface Transport {
+
     void Move();
-    void FuelUp();
-    String getModel();
-    void setModel(String model);
-    int getSpeed();
-    void setSpeed(int speed);
+    void Fuel();
+
 }
 
 class Car implements Transport {
-    private String model;
-    private int speed;
+    protected String model;
+    protected int speed;
+
+    Car(String model, int speed) {
+        this.model = model;
+        this.speed = speed;
+    }
 
     @Override
     public void Move() {
-        System.out.println("Автомобиль " + model + " едет со скоростью " + speed + " км/ч.");
+        System.out.println("Машина " + model + " едет со скоростью " + speed + " км/ч");
     }
 
     @Override
-    public void FuelUp() {
-        System.out.println("Заправляем автомобиль " + model + " бензином.");
-    }
-
-    @Override
-    public String getModel() {
-        return model;
-    }
-
-    @Override
-    public void setModel(String model) {
-        this.model = model;
-    }
-
-    @Override
-    public int getSpeed() {
-        return speed;
-    }
-
-    @Override
-    public void setSpeed(int speed) {
-        this.speed = speed;
+    public void Fuel() {
+        System.out.println("Использует бензин");
     }
 }
 
 class Motorcycle implements Transport {
-    private String model;
-    private int speed;
+    protected String model;
+    protected int speed;
+
+    Motorcycle(String model, int speed) {
+        this.model = model;
+        this.speed = speed;
+    }
 
     @Override
     public void Move() {
-        System.out.println("Мотоцикл " + model + " едет со скоростью " + speed + " км/ч.");
+        System.out.println("Мотоцикл " + model + " едет со скоростью " + speed + " км/ч");
     }
 
     @Override
-    public void FuelUp() {
-        System.out.println("Заправляем мотоцикл " + model + " бензином.");
-    }
-
-    @Override
-    public String getModel() {
-        return model;
-    }
-
-    @Override
-    public void setModel(String model) {
-        this.model = model;
-    }
-
-    @Override
-    public int getSpeed() {
-        return speed;
-    }
-
-    @Override
-    public void setSpeed(int speed) {
-        this.speed = speed;
+    public void Fuel() {
+        System.out.println("Использует бензин");
     }
 }
 
 class Plane implements Transport {
-    private String model;
-    private int speed;
+    protected String model;
+    protected int speed;
+
+    Plane(String model, int speed) {
+        this.model = model;
+        this.speed = speed;
+    }
 
     @Override
     public void Move() {
-        System.out.println("Самолет " + model + " летит со скоростью " + speed + " км/ч.");
+        System.out.println("Самолет " + model + " летит со скоростью " + speed + " км/ч");
     }
 
     @Override
-    public void FuelUp() {
-        System.out.println("Заправляем самолет " + model + " авиационным топливом.");
+    public void Fuel() {
+        System.out.println("Использует керосин");
     }
+}
 
-    @Override
-    public String getModel() {
-        return model;
-    }
+class Bike implements Transport {
+    protected String model;
+    protected int speed;
 
-    @Override
-    public void setModel(String model) {
+    Bike(String model, int speed) {
         this.model = model;
-    }
-
-    @Override
-    public int getSpeed() {
-        return speed;
-    }
-
-    @Override
-    public void setSpeed(int speed) {
         this.speed = speed;
+    }
+
+    @Override
+    public void Move() {
+        System.out.println("Велосипед " + model + " едет со скоростью " + speed + " км/ч");
+    }
+
+    @Override
+    public void Fuel() {
+        System.out.println("Использует энергию человека");
+    }
+}
+
+abstract class TransportFactory {
+    public abstract Transport CreateTransport(String model, int speed);
+}
+
+class CarFactory extends TransportFactory {
+    @Override
+    public Transport CreateTransport(String model, int speed) {
+        return new Car(model, speed);
+    }
+}
+
+class MotorcycleFactory extends TransportFactory {
+    @Override
+    public Transport CreateTransport(String model, int speed) {
+        return new Motorcycle(model, speed);
+    }
+}
+
+class PlaneFactory extends TransportFactory {
+    @Override
+    public Transport CreateTransport(String model, int speed) {
+        return new Plane(model, speed);
+    }
+}
+
+class BikeFactory extends TransportFactory {
+    @Override
+    public Transport CreateTransport(String model, int speed) {
+        return new Bike(model, speed);
     }
 }
